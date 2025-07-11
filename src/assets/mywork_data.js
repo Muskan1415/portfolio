77caa64d96f8c2666aec97b90e719379fb17773a
Muskan1415 <147226829+Muskan1415@users.noreply.github.com>
2025-07-11 19:29:10 +0530

    created my Work section and added necessary css

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,41 +1,41 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+import project4 from '../assets/project4.png';
+import project5 from '../assets/project5.png';
+import project6 from '../assets/project6.png';
 
 const mywork_data = [
     {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1_img
+        w_no: 1,
+        w_name: "Web design",
+        w_img: project1
     },
     {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
+        w_no: 2,
+        w_name: "Web design",
+        w_img: project2
     },
     {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
+        w_no: 3,
+        w_name: "Web design",
+        w_img: project3
     },
     {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
+        w_no: 4,
+        w_name: "Web design",
+        w_img: project4
     },
     {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
+        w_no: 5,
+        w_name: "Web design",
+        w_img: project5
     },
     {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
-    },
-]
- 
+        w_no: 6,
+        w_name: "Web design",
+        w_img: project6
+    }
+];
+
 export default mywork_data;
